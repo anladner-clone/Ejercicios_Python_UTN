383627ee8691636be0eb16b954529d31180288a4
ladner-ariel <anladner.work@gmail.com>
2024-12-02 04:02:16 -0300

    Actualizar pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,16 @@
 
-def pares(lista):
-    for i in lista:
-        for j in lista:
-            print(f"({i}, {j})")
+def busqueda_binaria(lista, objetivo):
+    izquierda, derecha = 0, len(lista) - 1
+    while izquierda <= derecha:
+        medio = (izquierda + derecha) // 2
+        if lista[medio] == objetivo:
+            return True
+        elif lista[medio] < objetivo:
+            izquierda = medio + 1
+        else:
+            derecha = medio - 1
+    return False
 
-pares([1, 2, 3])
-
-
+print(busqueda_binaria([1, 2, 3, 4, 5, 6], 4))  # True
 
 .
