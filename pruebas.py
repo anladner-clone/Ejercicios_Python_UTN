7e162fefd03de8b83a3948af399404b9269c994a
ladner-ariel <anladner.work@gmail.com>
2024-12-02 03:38:15 -0300

    Actualizar pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,3 @@
 
-def buscar_elemento(lista, elemento):
-    for i in lista:
-        if i == elemento:
-            return True
-    return False
-
-lista = [1, 2, 3, 4, 5, 6]
-print(buscar_elemento(lista, 4))  # True
-
-
-
-print(' ')
+lista = [10, 20, 30, 40, 50]
+print(lista[2])  # 30
