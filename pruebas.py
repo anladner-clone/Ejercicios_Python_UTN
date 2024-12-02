93a0f97fbb30cabcc91039601755bcf1fcea1e6d
ladner-ariel <anladner.work@gmail.com>
2024-12-02 04:02:47 -0300

    Actualizar pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -11,6 +11,6 @@ def busqueda_binaria(lista, objetivo):
             derecha = medio - 1
     return False
 
-print(busqueda_binaria([1, 2, 3, 4, 5, 6], 4))  # True
+print(busqueda_binaria([1, 2, 3, 4, 5, 6], 4))
 
 .
